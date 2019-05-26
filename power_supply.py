822f7bf3dd61421b9cb3398ec1d2c777bee73f5e
Luis Larco <luis@luislarco.com>
2019-05-26 01:02:18 -0700

    Add methods to power supply interface.

--- power_supply.py
+++ power_supply.py
@@ -3,4 +3,43 @@ import abc
 
 class PowerSupplyInterface(abc.ABC):
   """Base class for interfacing with a power supply."""
-  pass
+
+  @abc.abstractmethod
+  def set_voltage_level(self, voltage):
+    """Sets the voltage level."""
+    pass
+
+  @abc.abstractmethod
+  def set_current_level(self, current):
+    """Sets the current level."""
+    pass
+
+  @abc.abstractmethod
+  def get_voltage_level(self):
+    """Returns the voltage level setting value."""
+    pass
+
+  @abc.abstractmethod
+  def get_current_level(self):
+    """Returns the current level setting value."""
+    pass
+
+  @abc.abstractmethod
+  def get_display_voltage(self):
+    """Returns the display voltage."""
+    pass
+
+  @abc.abstractmethod
+  def get_display_current(self):
+    """Returns the display current."""
+    pass
+
+  @abc.abstractmethod
+  def set_output_on(self):
+    """Sets the output to ON."""
+    pass
+
+  @abc.abstractmethod
+  def set_output_off(self):
+    """Sets the output to OFF."""
+    pass
--- power_supply_test.py
+++ power_supply_test.py
@@ -1,7 +1,12 @@
 import unittest
 
-class TestPowerSupplyMethods(unittest.TestCase):
-  pass
+import power_supply
+
+class TestPowerSupplyAbstractClass(unittest.TestCase):
+
+  def test_instantiate_abstract_interface(self):
+    with self.assertRaises(TypeError):
+      power_supply_interface = power_supply.PowerSupplyInterface()
 
 
 if __name__ == '__main__':
